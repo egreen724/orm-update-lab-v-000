d890c1af7551d2ce266e4b10e161aaec0d9e86ef
Erin Green <erin.y.green@gmail.com>
2018-11-27 01:21:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,16 +70,9 @@ class Student
   
     DB[:conn].execute(sql,name).map do |row|
       self.new_from_db(row)
-    end
+    end.first
   end
-  
-  def self.find_by_name(name)
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE name = ?
-      LIMIT 1
-    SQL
+
 
     DB[:conn].execute(sql,name).map do |row|
       self.new_from_db(row)
