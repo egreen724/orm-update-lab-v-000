7a061be4f74b1a6f5930eeb076e5d450313378ef
Erin Green <erin.y.green@gmail.com>
2018-11-27 01:21:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -72,12 +72,6 @@ class Student
       self.new_from_db(row)
     end.first
   end
-
-
-    DB[:conn].execute(sql,name).map do |row|
-      self.new_from_db(row)
-    end.first
-  end
   
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
